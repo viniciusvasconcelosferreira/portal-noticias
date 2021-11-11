173fb02617ab23972f5cc9ee127c7700ea2369ca
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-11-11 04:29:53 -0300

    Criando a conexão com o MySQL e recuperando dados

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,22 @@
 module.exports = function (app) {
+
+
+
     app.get('/noticias', function (req, res) {
-        res.render("noticias/noticias");
+
+        var mysql = require('mysql');
+
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function (error, result) {
+            res.send(result);
+        });
+
+        // res.render("noticias/noticias");
     });
 }
