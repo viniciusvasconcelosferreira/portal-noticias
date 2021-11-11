1bf16602e2f6342d6e95f9400e150413b41fd4ce
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-11-11 10:52:35 -0300

    Inserindo registros no banco de dados MySQL

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -7,5 +7,9 @@ module.exports = function () {
         connection.query('select * from noticias where id_noticia = 2', callback);
     };
 
+    this.salvarNoticia = function (noticia, connection, callback) {
+        connection.query('insert into noticias set ?', noticia, callback);
+    };
+
     return this;
 }
