992b71ad490f4a2d5c90f236fe03a101d69a641e
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-11-11 15:09:54 -0300

    Exibindo erros de validação com express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,19 @@ module.exports = function (application) {
     application.post('/noticias/salvar', function (req, res) {
         var noticia = req.body;
 
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório e deve conter entre 10 e 100 caracteres').notEmpty().len(10,100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty();
+        req.assert('noticia', 'Noticia é obrigatório').notEmpty();
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            res.render("admin/form_add_noticia", { validacao: errors });
+            return;
+        }
+
         var connection = application.config.dbConnection();
 
         var noticiasModel = new application.app.models.NoticiasDAO(connection);
@@ -14,6 +27,5 @@ module.exports = function (application) {
             res.redirect('/noticias');
         });
     });
-
 
 }
--- config/server.js
+++ config/server.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 var app = express();
 
 //MOTOR DE GERAÇÃO DE VIEWS
 app.set('view engine', 'ejs');
-
 app.set('views', './app/views');
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
 
 consign()
     .include('app/routes')
