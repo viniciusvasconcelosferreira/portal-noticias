ebbff97ce8125758ea8a4b0cdfa8509977095fc5
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-11-11 04:40:14 -0300

    Refactoring do método de conexão com o banco de dados MySQL

---
+++ config/dbConnection.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+module.exports = function () {
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'portal_noticias'
+    });
+}
