bf6b324391fa493e63dd9125e489e98a8aa756af
viniciusvasconcelosferreira <viniciusvasconcelos.ferreira@gmail.com>
2021-11-11 22:56:27 -0300

    Formantando datas com o moment.js

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,11 @@
-module.exports.index = function(appliction, req, res){
-    res.render("home/index");
+var moment = require('moment');
+
+module.exports.index = function (appliction, req, res) {
+    var connection = appliction.config.dbConnection();
+    var noticiasModel = new appliction.app.models.NoticiasDAO(connection);
+
+    noticiasModel.get5UltimasNoticias(function (error, result) {
+        res.render("home/index", { noticias: result ,moment:moment});
+    });
+
 }
